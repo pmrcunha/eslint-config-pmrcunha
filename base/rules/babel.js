7b4b3623a45d4f42da87345bca8759d2560e5fd8
Paulo Cunha <pc@pmrcunha.com>
2019-02-18 20:20:25 +0100

    Update: Turn off curly spaces rule

--- base/rules/babel.js
+++ base/rules/babel.js
@@ -5,7 +5,7 @@ module.exports = {
     'babel/new-cap': 1,
     'babel/camelcase': 1,
     'babel/no-invalid-this': 1,
-    'babel/object-curly-spacing': 1,
+    'babel/object-curly-spacing': 0,
     'babel/quotes': 0,
     'babel/semi': 1,
     'babel/no-unused-expressions': 1,
